44f1356e0f3e750ffd5a94c18fe04d21d9d0a614
python <25702166+wCmJ@users.noreply.github.com>
2018-12-05 18:41:40 +0800

    Update Heap.h

--- Heap.h
+++ Heap.h
@@ -47,7 +47,28 @@ void Heap_Minimum_Construct(T *data, size_t len) {
 	}
 }
 
+template<class T>
+void Heap_Ascend_Sort(T *data, int len) {
+	Heap_Maximum_Construct(data, len);
+	for (int i = len - 1; i >0;i--) {
+		T tmp = data[0];
+		data[0] = data[i];
+		data[i] = tmp;
+		Heap_Maximum_Node(data, 0, i);
+	}
 
+}
+
+template<class T>
+void Heap_Descend_Sort(T *data, int len) {
+	Heap_Minimum_Construct(data, len);
+	for (int i = len - 1; i > 0; i--) {
+		T tmp = data[0];
+		data[0] = data[i];
+		data[i] = tmp;
+		Heap_Minimum_Node(data, 0, i);
+	}
+}
 
 
 
